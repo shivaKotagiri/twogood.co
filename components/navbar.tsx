84692075db3580bf9798fec32be45e421406aab0
shivaKotagiri <shivakotagiri532@gmail.com>
2025-10-10 00:32:36 +0530

    added the menu component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,65 @@
 "use client";
 
-import { Menu, ShoppingCart } from "lucide-react";
-import { useEffect, useState } from "react";
+import { Menu, ShoppingCart, X } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 export default function Navbar() {
   const [hide, setHide] = useState(false);
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const container = useRef<HTMLDivElement | null>(null);
+  const timeline = useRef<gsap.core.Timeline | null>(null);
+
+  useGSAP(() => {
+    gsap.set("#menu-overlay", {
+      clipPath: 'polygon(0 0, 100% 0, 100% 0, 0 0)',
+    });
+    gsap.set(".categories", {
+      y: 100,
+      opacity: 0
+    })
+    gsap.set(".footer-section", {
+      opacity: 0,
+      y: 20
+    })
+    timeline.current = gsap.timeline({ paused: true }).to("#menu-overlay", {
+      delay: 0,
+      duration: 0.35,
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      ease: "power4.inOut"
+    }).to(".categories", {
+      y: 0,
+      opacity: 1,
+      stagger: 0.03,
+      duration: 0.5,
+      ease: "power4.inOut",
+    }).to(".footer-section", {
+      opacity: 1,
+      y: 0,
+      duration: 0.35,
+      stagger: 0.1,
+      ease: "power4.out"
+    })
+  }, { scope: container})
+
+  const handleClick = () => {
+    setMenuIsOpen(!menuIsOpen);
+  }
+
+  useEffect(() => {
+    if(timeline.current) {
+      if(menuIsOpen) {
+        timeline.current.play();
+      } else {
+        timeline.current.reverse();
+      }
+    }
+  }, [menuIsOpen])
 
   useEffect(() => {
     const handleScroll = () => {
-      if(window.scrollY > 100) {
+      if(window.scrollY > 100 && menuIsOpen) {
         setHide(true)
       } else {
         setHide(false);
@@ -20,22 +71,65 @@ export default function Navbar() {
 
   }, [])
   return (
-    <nav className="w-full fixed top-0 left-0 right-0 flex items-center justify-between pt-6 z-50 pointer-events-none">
-      <h2 id="logo" className="fixed top-5 left-5 uppercase font-black text-[31px] leading-[90%] flex flex-col z-50 p-1 cursor-pointer pointer-events-auto">
-        <span>two</span>
-        <span>good</span>
-        <span>co.</span>
-      </h2>
-      <div id="nav-options" className="flex uppercase text-[12px] ml-auto gap-15 pointer-events-auto">
-        <div id="nav-1" className={`hidden md:flex transition-opacity duration-100 ${hide ? "opacity-0 pointer-events-none": "opacity-100"} gap-15 px-4 py-2 transform -translate-x-[175px]`}>
-          <p className="cursor-pointer">Shop</p>
-          <p className="cursor-pointer">Wholesale</p>
-          <p className="cursor-pointer">Catering</p>
-          <p className="cursor-pointer">Donate</p>
+    <nav ref={container} style={{ fontFamily: "Helvetica Now Text, Lucida Sans, Tahoma, sans-serif" }} className="fixed w-screen h-screen overflow-hidden">
+      <div className={`${menuIsOpen ? 'text-white' : 'text-black'} fixed bg-transparent w-screen top-0 left-0 right-0 flex items-center justify-between pt-5 z-50 pointer-events-none transition-colors duration-300`}>
+          <h2 id="logo" className="fixed top-5 left-5 uppercase font-black text-[31px] leading-[90%] flex flex-col z-50 p-1 cursor-pointer pointer-events-auto">
+          <span>two</span>
+          <span>good</span>
+          <span>co.</span>
+        </h2>
+        <div id="nav-options" className="flex justify-start items-start uppercase text-[12px] ml-auto gap-17 pointer-events-auto">
+          <div id="nav-1" className={`hidden md:flex transition-opacity duration-100 ${hide ? "opacity-0 pointer-events-none": "opacity-100"} gap-17 px-4 py-2 transform -translate-x-[185px]`}>
+            <p className="cursor-pointer">Shop</p>
+            <p className="cursor-pointer">Wholesale</p>
+            <p className="cursor-pointer">Catering</p>
+            <p className="cursor-pointer">Donate</p>
+          </div>
+          <div id="nav-icons" className={` fixed top-4 right-2 flex gap-17 px-5 py-3 rounded-full z-50 cursor-pointer pointer-events-auto`}>
+            <div onClick={handleClick}>{menuIsOpen ? <X size={23} strokeWidth={1} /> : <Menu size={16} strokeWidth={1} />}</div>
+            <ShoppingCart size={18} strokeWidth={1} />
+          </div>
         </div>
-        <div id="nav-icons" className="fixed top-5 right-8 flex gap-15 bg-white px-5 py-3 rounded-full z-50 cursor-pointer pointer-events-auto">
-          <Menu size={16} strokeWidth={1} />
-          <ShoppingCart size={16} strokeWidth={1} />
+      </div>
+      <div id="menu-overlay" className="w-screen h-screen flex flex-col justify-between bg-black text-white">
+        <div>
+          <div style={{ fontFamily: "Futura Now Headline"}} className="font-[850] text-[2.5rem] leading-10 text-end flex flex-col mt-50 uppercase lg:text-[5rem] lg:font-[850] px-5 lg:leading-20">
+            <div className="overflow-hidden"><p className="categories">shop</p></div>
+            <div className="overflow-hidden"><p className="categories">wholesale</p></div>
+            <div className="overflow-hidden"><p className="categories">catering</p></div>
+            <div className="overflow-hidden"><p className="categories">impact</p></div>
+            <div className="overflow-hidden"><p className="categories">stories</p></div>
+            <div className="overflow-hidden"><p className="categories">about</p></div>
+            <div className="overflow-hidden"><p className="categories">contact</p></div>
+            <div className="overflow-hidden"><p className="categories">donate</p></div>
+          </div>
+        </div>
+        <div className="md:w-[60%] flex justify-between text-[12px] px-5 lg:-translate-y-10">
+          <div className="footer-section">
+            <div className="text-neutral-500 uppercase">connect with us</div>
+            <div className="flex flex-col mt-8 text-[15px]">
+              <div className="hover:text-neutral-500 transition-colors duration-250 cursor-pointer">Facebook</div>
+              <div className="hover:text-neutral-500 transition-colors duration-250 cursor-pointer">Instagram</div>
+              <div className="hover:text-neutral-500 transition-colors duration-250 cursor-pointer">Twitter</div>
+              <div className="hover:text-neutral-500 transition-colors duration-250 cursor-pointer">LinkedIn</div>
+              <div className="hover:text-neutral-500 transition-colors duration-250 cursor-pointer">Youtube</div>
+            </div>
+          </div>
+          <div className="footer-section">
+            <div className="text-neutral-500 uppercase">nitty gritties</div>
+            <div className="flex flex-col mt-8 text-[15px]">
+              <div className="hover:text-neutral-500 transition-colors duration-250 cursor-pointer">Good Things FAQs</div>
+              <div className="hover:text-neutral-500 transition-colors duration-250 cursor-pointer">Good Food FAQs</div>
+              <div className="hover:text-neutral-500 transition-colors duration-250 cursor-pointer">Good Places</div>
+            </div>
+          </div>
+          <div className="footer-section lg:-translate-x-30">
+            <div className="text-neutral-500 uppercase">get started</div>
+            <div className="flex flex-col mt-8 text-[15px]">
+              <div className="hover:text-neutral-500 transition-colors duration-250 cursor-pointer">Pathways</div>
+              <div className="hover:text-neutral-500 transition-colors duration-250 cursor-pointer">Careers</div>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
--- components/sweet-image.tsx
+++ components/sweet-image.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function SweetImage() {
   return (
-    <div className="relative flex flex-col items-center mt-8">
+    <div className="flex flex-col items-center mt-8">
       <Image
         src="https://cdn.sanity.io/images/w8f1ak3c/production/492561cd624e31a70ffbca1ac98e87a5871d1ac3-2880x1606.png/2bcc3b0bb22cc8373f3aae9aeb97de1e50d64eae2-2880x1606.png?rect=0,0,2880,1507&fp-x=0.5&fp-y=0.469258011362239&w=2560&h=1390&fit=crop&crop=focalpoint&auto=format"
         alt="Hero Image"
@@ -14,7 +14,7 @@ export default function SweetImage() {
         className="rounded-lg md:w-[100%]"
       />
 
-      <div className="absolute w-[183px] h-[183px]">
+      <div className="w-[183px] h-[183px]">
         <div className="relative w-full h-full">
           <Image 
             className="animate-spin-super-slow cursor-pointer" 
@@ -23,7 +23,7 @@ export default function SweetImage() {
             width={182} 
             height={182} 
           />
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-3xl font-bold uppercase pointer-events-none">
+          <div className="top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-3xl font-bold uppercase pointer-events-none">
             Buy
           </div>
         </div>
